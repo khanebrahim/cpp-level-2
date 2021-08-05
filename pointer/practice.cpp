27fabd4b3f9035253a28783ca9560a57f4212006
BengalDroid <bengaldroid@gmail.com>
2021-08-05 12:49:11 +0600

    size of int

---
+++ pointer/practice.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+  
+int main()
+{
+    int a = 10;
+    ++a = 20; // error
+    printf("a = %d", a);
+    getchar();
+    return 0;
+}
---
+++ pointer/sizeof.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int size;
+    cout<<"Enter number of elements:" <<endl;
+    cin>>size;
+    int A[size];
+    cout<<sizeof A<<endl;
+    return 0;
+}
