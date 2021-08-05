3feb34381a2404af4e414318ff3fcbd3f1e92989
BengalDroid <bengaldroid@gmail.com>
2021-08-05 16:21:51 +0600

    address p

--- pointer/pointer_moving.cpp
+++ pointer/pointer_moving.cpp
@@ -3,11 +3,8 @@ using namespace std;
 int main(){
     int A[5]{2,4,6,8,10};
     int *p=A;
-    cout<<*p<<endl;
-    p++;
-    cout<<*p<<endl;
-    p--;
-    cout<<*p<<endl;
-    
+    cout<<p<<endl;
+    cout<<p+2<<endl;
+
     return 0;
 }
